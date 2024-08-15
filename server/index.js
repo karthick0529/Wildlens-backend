ae2a6cbca56dad294db23fdbcaf6af2775448f73
KARTHICK-R <karthicknandha619@gmail.com>
2024-08-15 14:46:02 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -11,13 +11,14 @@ const app = require('./app')
 console.log("Connecting to MongoDb...")
 
 //Connect to MongoDB using mongoose
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Testing console
+console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
 console.log("process.env.PORT", process.env.PORT);
 
-app.listen(port, () => {
-  console.log(`Server is running on the port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on the port ${PORT}`);
 });
 
        
--- server/utils/config.js
+++ server/utils/config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 //Create all the necessary configuration variables
 // const MongoDB_URI = process.env.MONGODB_URI;
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 const JWT_SECRET =process.env.JWT_SECRET;
 // const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 const CLIENT_SITE_URL = process.env.CLIENT_SITE_URL;
@@ -14,7 +14,7 @@ const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
 
 module.exports = {
     // MongoDB_URI,
-    PORT,
+    // PORT,
     JWT_SECRET,
     CLIENT_SITE_URL,
     RAZORPAY_KEY_ID,
