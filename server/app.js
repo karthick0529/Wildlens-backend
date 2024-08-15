55bd8512d90d4706f31313245ea6067e5e73ea8c
KARTHICK-R <karthicknandha619@gmail.com>
2024-08-15 18:30:14 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -64,7 +64,7 @@ const config = require('./utils/config');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',  // Allow only this origin
+    origin: 'https://capstone-wildlens-tour.netlify.app/',  // Allow only this origin
     credentials: true                 // Allow cookies and authentication headers
 }));
 
