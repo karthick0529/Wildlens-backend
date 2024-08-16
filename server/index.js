896b94e21dfb775cf3d824e39e92de33f029c411
KARTHICK-R <karthicknandha619@gmail.com>
2024-08-16 19:24:54 +0530

    deleted comments

--- server/index.js
+++ server/index.js
@@ -1,29 +1,3 @@
-// //import the mongoose module
-// const mongoose = require("mongoose");
-
-// //import the config module
-// const config = require('./utils/config');
-// require('dotenv').config();
-
-
-// //import the app module
-// const app = require('./app')
-// console.log("Connecting to MongoDb...")
-
-// //Connect to MongoDB using mongoose
-// mongoose.connect(config.MongoDB_URI)
-//         .then (()=>{
-//              console.log("Connected To MongoDB...");
-//               //start the server 
-//         app.listen(config.PORT, ()=>{
-//             console.log(`server running on Port ${config.PORT}`)
-//         })
-//         } ).catch ((error)=> {
-//             console.log("Error connecting to MongoDB...",error.message)
-//         })
-
-       
-
 // server.js
 const mongoose = require("mongoose");
 const config = require('./utils/config');
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,25 +1,3 @@
-// //import the dotenv package
-// require('dotenv').config();
-
-
-// //Create all the necessary configuration variables
-// const MongoDB_URI = process.env.MONGODB_URI;
-// const PORT = process.env.PORT;
-// const JWT_SECRET =process.env.JWT_SECRET;
-// // const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-// const CLIENT_SITE_URL = process.env.CLIENT_SITE_URL;
-// const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID;
-// const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
-// //export the configuration variables
-
-// module.exports = {
-//     MongoDB_URI,
-//     PORT,
-//     JWT_SECRET,
-//     CLIENT_SITE_URL,
-//     RAZORPAY_KEY_ID,
-//     RAZORPAY_KEY_SECRET,
-// }
 
 // utils/config.js
 require('dotenv').config();
